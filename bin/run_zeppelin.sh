71e240f61316e81ae0e1411d77f3bcbeea63d135
Luis Eduardo Ferro Diez <darknesssamurai@gmail.com>
2019-06-28 19:17:43 -0500

    Add zeppelin interpreter configuration to the mounted volumes in the docker container. Add esri geometry library to spark interpreter's configuration

--- bin/run_zeppelin.sh
+++ bin/run_zeppelin.sh
@@ -3,4 +3,7 @@
 current_dir=$(pwd)
 project_dir=$(dirname ${current_dir})
 
-docker container run -it --rm -v ${project_dir}/notebooks/zeppelin/:/zeppelin/notebook -v ${project_dir}/datasets/:/zeppelin/datasets -p 8080:8080 -p 4040:4040 apache/zeppelin:0.8.1
+docker container run -it --rm -v ${project_dir}/notebooks/zeppelin/:/zeppelin/notebook \
+-v ${project_dir}/datasets/:/zeppelin/datasets \
+-v ${project_dir}/resources/zeppelin/conf/interpreter.json:/zeppelin/conf/interpreter.json \
+-p 8080:8080 -p 4040:4040 apache/zeppelin:0.8.1
