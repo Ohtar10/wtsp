183d5d3ba87fe36a555e530895cd1a2101bdd188
Luis Eduardo Ferro Diez <luisedof10@gmail.com>
2020-05-20 19:54:42 -0500

    Small fixes to tensorflow init, and quick refactorings for dependency handling

--- cli/wtsp/core/sklearn/transformers.py
+++ cli/wtsp/core/sklearn/transformers.py
@@ -600,7 +600,11 @@ def is_valid_polygon(location_column):
 def init_tensorflow():
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        # s_config = tf.ConfigProto()
-        # s_config.gpu_options.allow_growth = True
-        # s_config.gpu_options.per_process_gpu_memory_fraction = 0.6
-        # tf.keras.backend.set_session(tf.Session(config=s_config))
+        gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+        if gpu_devices:
+            from tensorflow.compat.v1 import ConfigProto, InteractiveSession
+            config = ConfigProto()
+            config.gpu_options.allow_growth = True
+            config.gpu_options.per_process_gpu_memory_fraction = 0.6
+            # Necessary to default a session in tensorflow for keras to grab
+            session = InteractiveSession(config=config)
