0b235be5cddee0d7c81a2a74ca2520152e132104
Luis Eduardo Ferro Diez <luisedof10@gmail.com>
2020-05-30 09:58:22 -0500

    * Change class and split character parameter name * Implement shuffle in write

--- dataprep/pyspark/df-sampling.py
+++ dataprep/pyspark/df-sampling.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, col, split, array_contains, concat_ws
+from pyspark.sql.functions import explode, col, split, array_contains, concat_ws, rand
 import argparse
 import sys
 
@@ -8,8 +8,8 @@ parser.add_argument("--fraction", help="Fraction of sample")
 parser.add_argument("--seed", help="Random seed for reproducibility")
 parser.add_argument("--input", help="Input path of the dataset to sample")
 parser.add_argument("--output", help="Output path to sink the result")
-parser.add_argument("--stratacol", help="Category column to keep the strata proportions")
-parser.add_argument("--explodechar", help="Specifying this parameter indicates the stratacol contains multiple values separated by this character")
+parser.add_argument("--class-col", help="Category column to keep the strata proportions")
+parser.add_argument("--split-char", help="Specifying this parameter indicates the class-col contains multiple values separated by this character")
 args = parser.parse_args()
 
 if not args.fraction:
@@ -21,45 +21,45 @@ if not args.input:
 if not args.output:
     sys.exit("The output path is required")
 
-if not args.stratacol:
+if not args.class_col:
     sys.exit("The strata column is required")
 
 
 def simple_sampling(df):
-    classes = list(map(lambda row: row[args.stratacol], df.select(args.stratacol).distinct().collect()))
+    classes = list(map(lambda row: row[args.class_col], df.select(args.class_col).distinct().collect()))
     if seed:
-        sample = df.filter(df[args.stratacol] == classes[0]).sample(fraction, seed)
+        sample = df.filter(df[args.class_col] == classes[0]).sample(fraction, seed)
     else:
-        sample = df.filter(df[args.stratacol] == classes[0]).sample(fraction)
+        sample = df.filter(df[args.class_col] == classes[0]).sample(fraction)
 
     for clazz in classes[1:]:
         if seed :
-            sample = sample.union(df.filter(df[args.stratacol] == clazz).sample(fraction, seed))
+            sample = sample.union(df.filter(df[args.class_col] == clazz).sample(fraction, seed))
         else:
-            sample = sample.union(df.filter(df[args.stratacol] == clazz).sample(fraction))
+            sample = sample.union(df.filter(df[args.class_col] == clazz).sample(fraction))
 
     return sample, len(classes)
 
 
 def explode_sampling(df):
     base_columns = df.columns
-    columns = list(map(lambda c: split(col(c), args.explodechar).alias(c) if c == args.stratacol else col(c), base_columns))
+    columns = list(map(lambda c: split(col(c), args.split_char).alias(c) if c == args.class_col else col(c), base_columns))
     with_category_array = df.select(*columns)
     df = df.select(columns)
-    classes = list(map(lambda row: row[args.stratacol], df.select(explode(args.stratacol).alias(args.stratacol)).distinct().collect()))
+    classes = list(map(lambda row: row[args.class_col], df.select(explode(args.class_col).alias(args.class_col)).distinct().collect()))
 
     if seed:
-        sample = with_category_array.filter(array_contains(with_category_array[args.stratacol], classes[0])).sample(fraction, seed)
+        sample = with_category_array.filter(array_contains(with_category_array[args.class_col], classes[0])).sample(fraction, seed)
     else:
-        sample = with_category_array.filter(array_contains(with_category_array[args.stratacol], classes[0])).sample(fraction)
+        sample = with_category_array.filter(array_contains(with_category_array[args.class_col], classes[0])).sample(fraction)
 
     for clazz in classes[1:]:
         if seed :
-            sample = sample.union(with_category_array.filter(array_contains(with_category_array[args.stratacol], clazz)).sample(fraction, seed))
+            sample = sample.union(with_category_array.filter(array_contains(with_category_array[args.class_col], clazz)).sample(fraction, seed))
         else:
-            sample = sample.union(with_category_array.filter(array_contains(with_category_array[args.stratacol], clazz)).sample(fraction))
+            sample = sample.union(with_category_array.filter(array_contains(with_category_array[args.class_col], clazz)).sample(fraction))
 
-    select = list(map(lambda c: concat_ws(";", col(c)).alias(c) if c == args.stratacol else col(c), base_columns))
+    select = list(map(lambda c: concat_ws(";", col(c)).alias(c) if c == args.class_col else col(c), base_columns))
     return sample.select(select), len(classes)
 
 
@@ -69,9 +69,9 @@ df = spark.read.parquet(args.input)
 fraction = float(args.fraction)
 seed = args.seed
 
-if not args.explodechar:
+if not args.split_char:
     sample, partitions = simple_sampling(df)
 else:
     sample, partitions = explode_sampling(df)
 
-sample.coalesce(partitions).write.mode("overwrite").parquet(args.output)
+sample.orderBy(rand()).coalesce(partitions).write.mode("overwrite").parquet(args.output)
